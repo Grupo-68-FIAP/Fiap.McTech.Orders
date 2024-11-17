aacb294b0a210964b9858b4b5c1cc9c2808a4ab7
guilherme_novaes <guilherme.novaes233@gmail.com>
2024-11-17 14:15:45 -0300

    criando base do projeto

---
+++ src/CrossCutting.Ioc/Mappers/AutoMapperConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using CrossCutting.Ioc.Mappers.Profiles;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CrossCutting.Ioc.Mappers
+{
+    public static class AutoMapperConfiguration
+    {
+        public static void RegisterMappings(this IServiceCollection services)
+        {
+            MapperConfiguration config = new(cfg =>
+            {
+                // Register Profiles
+                cfg.AddProfile<OrderProfile>();
+            });
+
+            IMapper mapper = config.CreateMapper();
+            services.AddSingleton(mapper);
+        }
+    }
+}
---
+++ src/CrossCutting.Ioc/NativeBootstrapInjector.cs
@@ -0,0 +1,21 @@
+﻿using Application.Interfaces;
+using AppServices.Orders;
+using CrossCutting.Ioc.Infra;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CrossCutting.Ioc
+{
+    public static class NativeBootstrapInjector
+    {
+        public static void RegisterServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Infra 
+            services.ConfigureSqlServer(configuration);
+            services.RegisterRepositories();
+
+            //APP Services 
+            services.AddScoped<IOrderAppService, OrderAppService>(); 
+        }
+    }
+}
