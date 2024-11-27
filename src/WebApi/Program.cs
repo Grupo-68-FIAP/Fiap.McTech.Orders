7c9ba905e5745f0274e0b764fec91b3046ea34fb
guilherme_novaes <guilherme.novaes233@gmail.com>
2024-11-26 22:53:31 -0300

    feature: adding opendId and swagger config

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,24 +1,39 @@
 using CrossCutting.Ioc;
+using CrossCutting.Ioc.Infra;
+using CrossCutting.Ioc.Mappers;
+using WebApi.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers(); 
+// Add services to the container.
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddJwtBearerAuthentication();
+builder.Services.AddSwagger();
 
-builder.Services.RegisterServices(builder.Configuration); 
+// AutoMapper configuration
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.RegisterMappings();
+
+builder.Services.RegisterServices(builder.Configuration);
+
+// Cors configuration
+builder.Services.AddCors(options =>
+{
+    var allowOrigins = builder.Configuration.GetValue<string>("ALLOW_ORIGINS") ?? "*";
+    options.AddPolicy("CorsConfig", builder => builder.WithOrigins(allowOrigins.Split(';')).AllowAnyMethod().AllowAnyHeader());
+});
+
 
 var app = builder.Build();
-if (app.Environment.IsDevelopment())
-{
-	app.UseSwagger();
-	app.UseSwaggerUI();
-}
 
-app.UseHttpsRedirection();
+using var scope = app.Services.CreateScope();
+scope.McTechDatabaseInitialize();
 
-app.UseAuthorization();
+app.UseSwaggerV3();
+app.UseCors("CorsConfig"); 
+app.UseAuth();
 
 app.MapControllers();
 
-app.Run();
+await app.RunAsync();
