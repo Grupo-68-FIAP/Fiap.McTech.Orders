e71298aea18e3e464314ecc1bea6c298e6fa117e
Felipe Tomm <felipe.tomm@gmail.com>
2024-12-02 23:24:49 -0300

    Fix AlternateAttribute

--- src/Domain/Enums/OrderStatus.cs
+++ src/Domain/Enums/OrderStatus.cs
@@ -6,25 +6,18 @@ namespace Domain.Enums
     public enum OrderStatus
     {
 		[Description("Nenhum")]
-        [AlternateValue("NONE")]
         None = -1,
         [Description("Aguardando Pagamento")]
-        [AlternateValue("WAITING_PAYMENT")]
         WaitPayment,
         [Description("Recebido")]
-        [AlternateValue("RECEIVED")]
         Received,
         [Description("Em Preparo")]
-        [AlternateValue("IN_PROGRESS")]
         InPreparation,
         [Description("Pronto")]
-        [AlternateValue("READY")]
         Ready,
         [Description("Finalizado")]
-        [AlternateValue("FINALIZED")]
         Finished,
         [Description("Cancelado")]
-        [AlternateValue("CANCELLED")]
         Canceled = -2
     }
 }
--- src/Domain/Utils/Attributes/AlternateAttribute.cs
+++ src/Domain/Utils/Attributes/AlternateAttribute.cs
@@ -1,5 +1,6 @@
 namespace Domain.Utils.Attributes
 {
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Enum | AttributeTargets.Interface | AttributeTargets.Delegate)]
     public class AlternateValueAttribute : Attribute
     {
         public string AlternateValue { get; protected set; }
