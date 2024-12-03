775ab2425649ff8caec9538ba1cfa0fa7f818f02
Felipe Tomm <felipe.tomm@gmail.com>
2024-12-02 23:18:00 -0300

    FIX: Add alternate value

--- src/Domain/Enums/OrderStatus.cs
+++ src/Domain/Enums/OrderStatus.cs
@@ -1,22 +1,30 @@
-﻿using System.ComponentModel;
+﻿using Domain.Utils.Attributes;
+using System.ComponentModel;
 
 namespace Domain.Enums
 {
     public enum OrderStatus
     {
 		[Description("Nenhum")]
+        [AlternateValue("NONE")]
         None = -1,
         [Description("Aguardando Pagamento")]
+        [AlternateValue("WAITING_PAYMENT")]
         WaitPayment,
         [Description("Recebido")]
+        [AlternateValue("RECEIVED")]
         Received,
         [Description("Em Preparo")]
+        [AlternateValue("IN_PROGRESS")]
         InPreparation,
         [Description("Pronto")]
+        [AlternateValue("READY")]
         Ready,
         [Description("Finalizado")]
+        [AlternateValue("FINALIZED")]
         Finished,
         [Description("Cancelado")]
+        [AlternateValue("CANCELLED")]
         Canceled = -2
     }
 }
